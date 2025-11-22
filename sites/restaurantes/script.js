f61e59aefef07d4a1d643c2ebfae7544de323a01
TartVigas <thiagobarrosvend@gmail.com>
2025-11-22 13:37:08 -0300

    Delete sites/restaurantes directory

--- sites/restaurantes/script.js
+++
@@ -1,3 +0,0 @@
-// Deixa aqui para futuras interações (scroll suave, menu mobile, etc)
-// Por enquanto, o template funciona todo em HTML + CSS.
-console.log("Template Restaurante Gourmet BRsys carregado");
