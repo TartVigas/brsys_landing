5c67c6bf0a567e08286ea1dfca0cc472c5b1a0bb
TartVigas <thiagobarrosvend@gmail.com>
2025-11-22 09:19:02 -0300

    Add initial script.js for restaurant template

---
+++ sites/restaurantes/script.js
@@ -0,0 +1,3 @@
+// Deixa aqui para futuras interações (scroll suave, menu mobile, etc)
+// Por enquanto, o template funciona todo em HTML + CSS.
+console.log("Template Restaurante Gourmet BRsys carregado");
